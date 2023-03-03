0cf75bbab8a93949c2a074fcbcbae8710bc341f0
Fernando Costa <ferndcosta353@gmail.com>
2023-03-03 16:24:51 -0300

    feat: Add functionality to increment post likes
    adds the hability to like posts using the heart icon as a button. Every time the user clicks the number of likes goes up

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,42 +33,62 @@ const posts = [
 
 const postsSectionEl = document.getElementById('posts-section');
 
-let allPosts = ""
+let allPosts = "";
 
 for (let i = 0; i < posts.length; i++) {
-    allPosts += `
+  allPosts += `
     <article class="container">
-        <div class="post-header">
-            <img src=${posts[i].avatar} class="post-user-avatar">
-            <div class="flex-group">
-                <a href="#" class="post-username">${posts[i].name}</a>
-                <a href="#" class="post-location fs-small fw-regular">${posts[i].location}</a>
-            </div>
+      <div class="post-header">
+        <img src=${posts[i].avatar} class="post-user-avatar">
+        <div class="flex-group">
+          <a href="#" class="post-username">${posts[i].name}</a>
+          <a href="#" class="post-location fs-small fw-regular">${posts[i].location}</a>
         </div>
+      </div>
 
-        <img src=${posts[i].post} alt="${posts[i].alt}">
+      <img src=${posts[i].post} alt="${posts[i].alt}">
 
-        <div class="post-footer">
-            <ul class="post-icons-ul">
-                <li>
-                    <button class="post-icon-button"><img src="assets/img/icons/icon-heart.png" alt="Heart icon"></button>
-                </li>
-                <li>
-                    <button class="post-icon-button"><img src="assets/img/icons/icon-comment.png" alt="Comment icon"></button>
-                </li>
-                <li>
-                    <button class="post-icon-button"><img src="assets/img/icons/icon-dm.png" alt="Private message icon"></button>
-                </li>
-            </ul>
+      <div class="post-footer">
+        <ul class="post-icons-ul">
+          <li>
+            <!-- added a "data-index" attribute to identify the index of the post object in the array -->
+            <button class="post-icon-button icon-heart" data-index=${i}><img src="assets/img/icons/icon-heart.png" alt="Heart icon"></button>
+          </li>
+          <li>
+            <button class="post-icon-button"><img src="assets/img/icons/icon-comment.png" alt="Comment icon"></button>
+          </li>
+          <li>
+            <button class="post-icon-button"><img src="assets/img/icons/icon-dm.png" alt="Private message icon"></button>
+          </li>
+        </ul>
 
-            <p class="post-likes">${posts[i].likes}</p>
-            <div class="comment-section">
-                <a href="#" class="comment-username">${posts[i].username}</a>
-                <p class="post-comment fw-regular">${posts[i].comment}</p>
-            </div>
+        <!-- added a "data-index" attribute to identify the index of the post object in the array -->
+        <p class="post-likes" data-index=${i}>${posts[i].likes}</p>
+        <div class="comment-section">
+          <a href="#" class="comment-username">${posts[i].username}</a>
+          <p class="post-comment fw-regular">${posts[i].comment}</p>
         </div>
+      </div>
     </article>
-    `
+  `;
 }
 
 postsSectionEl.innerHTML = allPosts;
+
+const heartIcons = document.querySelectorAll('.post-icon-button.icon-heart');
+
+heartIcons.forEach(heartIcon => {
+    
+    // gets the index of the post object in the array using the "data-index" attribute
+    const index = heartIcon.getAttribute('data-index');
+
+    heartIcon.addEventListener('click', () => {
+
+    // increments the "likes" property of the post object at the specified index
+    posts[index].likes++;
+    
+    // updates the text content of the "post-likes" element at the specified index
+    document.querySelector(`.post-likes[data-index="${index}"]`).textContent = posts[index].likes;
+  });
+
+});

b69e2e5f2438c9823379e2b5fb48b501979be977
Fernando Costa <ferndcosta353@gmail.com>
2023-03-08 21:16:55 -0300

    feat: adds data file containing 'postsData' array

---
+++ assets/js/data.js
@@ -0,0 +1,32 @@
+export const postsData = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "/assets/img/posts/avatar-vangogh.jpg",
+        post: "/assets/img/posts/post-vangogh.jpg",
+        alt: "Vincent van Gogh avatar picture",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "/assets/img/posts/avatar-courbet.jpg",
+        post: "/assets/img/posts/post-courbet.jpg",
+        alt: "Gustave Coubert avatar picture",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "/assets/img/posts/avatar-ducreux.jpg",
+        post: "/assets/img/posts/post-ducreux.jpg",
+        alt: "Josesh Ducreux avatar picture",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
